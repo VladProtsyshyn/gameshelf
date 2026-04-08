3b8a1e3c75ddf35d5248056426aef585f1deb62e
VladProtsyshyn <bullgray.com@gmail.com>
2026-04-08 17:30:31 +0300

    chore: prepare app for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import SavedGamesProvider from './features/games/context/SavedGamesContext'
 import './styles/global.css'
@@ -8,9 +8,9 @@ import './styles/global.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SavedGamesProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </SavedGamesProvider>
   </React.StrictMode>,
 )
